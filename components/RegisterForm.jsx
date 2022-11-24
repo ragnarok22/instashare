2f9d432205706cc37318bd6edb790a138ca86317
Reinier Hernández <sasuke.reinier@gmail.com>
2022-11-24 10:33:02 -0500

    feat: get info from form in login and register

--- components/RegisterForm.jsx
+++ components/RegisterForm.jsx
@@ -1,38 +1,62 @@
 import Link from "next/link"
+import { useState } from "react"
 
 const RegisterForm = () => {
+  const [name, setName] = useState()
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+  const [confirmPassword, setConfirmPassword] = useState()
+  const [loading, setLoading] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setLoading(true)
+
+    // create user
+  }
+
   return (
     <div className="bg-gray-100 min-h-screen flex flex-col">
       <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-        <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+        <form className="bg-white px-6 py-8 rounded shadow-md text-black w-full" onSubmit={handleSubmit}>
           <h1 className="mb-8 text-3xl text-center">Sign up</h1>
           <input
             type="text"
             className="block border border-gray-500 w-full p-3 rounded mb-4"
             name="fullname"
+            value={name}
+            onChange={e => setName(e.target.value)}
             placeholder="Full Name" />
 
           <input
             type="text"
             className="block border border-gray-500 w-full p-3 rounded mb-4"
             name="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
             placeholder="Email" />
 
           <input
             type="password"
             className="block border border-gray-500 w-full p-3 rounded mb-4"
             name="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
             placeholder="Password" />
           <input
             type="password"
             className="block border border-gray-500 w-full p-3 rounded mb-4"
             name="confirm_password"
+            value={confirmPassword}
+            onChange={e => setConfirmPassword(e.target.value)}
             placeholder="Confirm Password" />
 
           <button
             type="submit"
             className="w-full text-center py-3 rounded bg-green-500 text-white hover:bg-green-700 focus:outline-none my-1"
-          >Create Account</button>
+          >
+            {loading ? "Loading" : "Create Account"}
+          </button>
 
           <div className="text-center text-sm text-gray-700 mt-4">
             By signing up, you agree to the  {" "}
@@ -43,7 +67,7 @@ const RegisterForm = () => {
               Privacy Policy  {" "}
             </a>
           </div>
-        </div>
+        </form>
 
         <div className="text-gray-700 mt-6">
           Already have an account?  {" "}
