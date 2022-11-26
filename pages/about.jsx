f92aad3a3e28cbb42cf745e42fd6fafd8f526d32
Reinier Hernández <sasuke.reinier@gmail.com>
2022-11-26 10:30:39 -0500

    feat: add about page

---
+++ pages/about.jsx
@@ -0,0 +1,12 @@
+import DashboardLayout from "../layouts/DashboardLayout"
+
+export default function About() {
+  return (
+    <DashboardLayout>
+      <div className="text-center mt-4 text-lg font-medium">
+        Unload and download files.
+      </div>
+    </DashboardLayout>
+
+  )
+}
