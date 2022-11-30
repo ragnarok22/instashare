b4e4a33e1f733b6c1ed28a02ed31c48f89ce9ff1
Reinier Hernández <sasuke.reinier@gmail.com>
2022-11-30 10:55:26 -0500

    perf: footer appearance

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,8 @@
 const Footer = () => (
-  <footer className="w-full text-center py-1 mt-auto bg-white text-gray-600">
-    Create by <a href="https://ragnarok22.dev" target="_blank" rel="noreferrer" className="text-blue-500 underline">Ragnarok</a>
+  <footer className="w-full text-center px-2 mt-auto text-gray-600 mb-2">
+    <div className="w-full bg-white py-2 rounded-xl shadow-md">
+      Create by <a href="https://ragnarok22.dev" target="_blank" rel="noreferrer" className="text-blue-500 underline">Ragnarok</a>
+    </div>
   </footer>
 )
 
