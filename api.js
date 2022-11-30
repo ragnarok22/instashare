87713c8860f02fc8a63b8b5e4debee438081dca5
Reinier Hernández <sasuke.reinier@gmail.com>
2022-11-30 11:34:08 -0500

    perf: added interceptors to axios to redirect to login when the request is Unauthorized

--- api.js
+++ api.js
@@ -3,16 +3,40 @@ import axios from "axios";
 const baseURL =
   process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/" + "api/";
 
-const axios_instance = axios.create({
-  baseURL,
-  headers: {
-    Accept: "application/json",
+// set default values
+axios.defaults.baseURL = baseURL;
+axios.defaults.headers.common["Accept"] = "application/json";
+
+// add a request interceptor
+axios.interceptors.request.use(
+  (config) => {
+    return config;
   },
-});
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// add a response interceptor
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    // redirect to login when a request raises an Unauthorized error
+    if (error.response.status === 401) {
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
 
 const apiSettings = {
+  setToken: (token) => {
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  },
   login: async (data) => {
-    const response = await axios_instance
+    const response = await axios
       .post("login/", data)
       .then((response) => {
         return response;
@@ -26,21 +50,21 @@ const apiSettings = {
     return response;
   },
   signup: async (data) => {
-    const response = await axios_instance
+    const response = await axios
       .post("register/", data)
       .then((response) => response)
       .catch((error) => error.response);
     return response;
   },
   logout: async () => {
-    const response = await axios_instance
+    const response = await axios
       .post("logout/", {})
       .then((response) => response)
       .catch((error) => error.response);
     return response;
   },
   listFiles: async (token) => {
-    const response = await axios_instance
+    const response = await axios
       .get("files/", {
         headers: {
           Authorization: `Bearer ${token}`,
@@ -62,7 +86,7 @@ const apiSettings = {
       form_data.append("file", data.file, data.file.name);
       form_data.append("title", data.title);
 
-      const newFile = await axios_instance
+      const newFile = await axios
         .post("files/", form_data, {
           headers: {
             "Content-Type": "multipart/form-data",
@@ -83,7 +107,7 @@ const apiSettings = {
     }
   },
   deleteFile: async (file_id, token) => {
-    const response = await axios_instance
+    const response = await axios
       .delete(`files/${file_id}`, {
         headers: {
           Authorization: `Bearer ${token}`,
@@ -100,7 +124,7 @@ const apiSettings = {
   updateFile: async (file_id, title, token) => {
     let form_data = new FormData();
     form_data.append("title", title);
-    const response = await axios_instance
+    const response = await axios
       .patch(`files/${file_id}/`, form_data, {
         headers: {
           "Content-Type": "multipart/form-data",
@@ -116,7 +140,7 @@ const apiSettings = {
     return response;
   },
   downloadAll: async (token) => {
-    const response = await axios_instance
+    const response = await axios
       .get("/files/compress/", {
         headers: {
           Authorization: `Bearer ${token}`,
@@ -129,7 +153,7 @@ const apiSettings = {
     return response;
   },
   checkDownload: async (token) => {
-    const response = await axios_instance
+    const response = await axios
       .get("/files/compress/check/", {
         headers: {
           Authorization: `Bearer ${token}`,
@@ -140,7 +164,7 @@ const apiSettings = {
     return response;
   },
   forceDownload: async (token) => {
-    const response = await axios_instance
+    const response = await axios
       .get("/files/compress/force", {
         headers: {
           Authorization: `Bearer ${token}`,
@@ -151,7 +175,7 @@ const apiSettings = {
     return response;
   },
   getUser: async (token) => {
-    const response = await axios_instance
+    const response = await axios
       .get("users/me", {
         headers: {
           Authorization: `Bearer ${token}`,
@@ -162,7 +186,7 @@ const apiSettings = {
     return response;
   },
   updateUser: async (user_id, data, token) => {
-    const response = await axios_instance
+    const response = await axios
       .patch(`/users/${user_id}/`, data, {
         headers: {
           Authorization: `Bearer ${token}`,
